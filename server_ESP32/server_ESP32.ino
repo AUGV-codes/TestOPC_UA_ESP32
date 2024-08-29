de509a2c057ca1240b556ef5529d863c268f5c95
AgustínGtz <agustin_gutierrez@d-automation.com>
2024-08-29 17:56:04 -0600

    -Se pudo dar de alta y realizar la escritura de registros -Se pudo igualmente leer varios datos de las memorias de registros y bobinas

--- server_ESP32/server_ESP32.ino
+++ server_ESP32/server_ESP32.ino
@@ -19,17 +19,30 @@ byte mac[] = {
 IPAddress ip(192, 168, 1, 202); // The IP address will be dependent on your local network:
 ModbusEthernet mb;              // Declare ModbusTCP instance
 
+int IntegerNumAddr=100, DecimalNumAddr=101, activationAddr=5;
 
 void setup() {
   Serial.begin(115200);     // Open serial communications and wait for port to open
   Ethernet.init(2);        // SS pin
   Ethernet.begin(mac, ip);  // start the Ethernet connection
+  while (Ethernet.linkStatus() != LinkON){          //Wait for conection with the net
+    delay(500);
+    Serial.print(".");
+  }
   delay(1000);              // give the Ethernet shield a second to initialize
   mb.server();              // Act as Modbus TCP server
-  mb.addReg(HREG(100));     // Add Holding register #100
+  mb.addReg(HREG(IntegerNumAddr));     // Add Holding register #100
+  mb.addReg(HREG(DecimalNumAddr));     // Add Holding register #101
+  mb.addCoil(activationAddr);
 }
 
 void loop() {
   mb.task();                // Server Modbus TCP queries
+  Serial.print("El numero completo es: ");
+  Serial.print(mb.Reg(HREG(IntegerNumAddr)));                                       //read the register 100
+  Serial.print(".");
+  Serial.println(mb.Reg(HREG(DecimalNumAddr)));                                     //read the register 101
+  Serial.print("Se activa la señal: ");
+  Serial.println(mb.Coil(activationAddr));
   delay(50);
 }
